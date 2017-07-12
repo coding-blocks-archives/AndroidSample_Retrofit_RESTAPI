872d24ccc7c68a75614f5c0a0f7a530634a2ffaa
Arnav Gupta <arnav@codingblocks.com>
2017-07-12 21:16:56 +0530

    use kotlin for models
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/restapi/models/Models.kt
@@ -0,0 +1,13 @@
+package com.codingblocks.restapi.models
+
+/**
+ * Created by championswimmer on 12/07/17.
+ */
+
+data class Album(val id: Int, val userId: Int, val title: String)
+
+data class Post(val userId: Int, val id: Int, val title: String, val body: String)
+
+data class User(val id: Int, val name: String, val username: String, val email: String, val phone: String)
+
+data class Todo(val id: Int, val userId: Int, val title: String, val isCompleted: Boolean)
