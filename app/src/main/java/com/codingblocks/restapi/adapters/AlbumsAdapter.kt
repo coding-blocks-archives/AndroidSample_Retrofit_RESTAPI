21319ef867b963eb36710e77b8e8c0004ec0d215
Arnav Gupta <arnav@codingblocks.com>
2017-07-12 21:19:12 +0530

    automatic conversion of adapters
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/restapi/adapters/AlbumsAdapter.kt
@@ -0,0 +1,49 @@
+package com.codingblocks.restapi.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.codingblocks.restapi.R
+import com.codingblocks.restapi.models.Album
+
+import java.util.ArrayList
+
+/**
+ * Created by championswimmer on 12/07/17.
+ */
+
+class AlbumsAdapter(private var albums: ArrayList<Album>?) : RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>() {
+
+    fun setAlbums(albums: ArrayList<Album>) {
+        this.albums = albums
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+        val li = LayoutInflater.from(parent.context)
+        return AlbumViewHolder(li.inflate(R.layout.list_item_album, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+        holder.bindView(albums!![position])
+    }
+
+    override fun getItemCount(): Int {
+        return albums!!.size
+    }
+
+    internal inner class AlbumViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvAlbumTitle: TextView
+
+        init {
+            tvAlbumTitle = itemView.findViewById<View>(R.id.tvAlbumTitle) as TextView
+        }
+
+        fun bindView(album: Album) {
+            tvAlbumTitle.text = album.title
+        }
+    }
+}
