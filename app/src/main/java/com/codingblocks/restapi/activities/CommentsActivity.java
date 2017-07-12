a74ef266d3af5ffe923f296bc41d80fd050b9677
Arnav Gupta <arnav@codingblocks.com>
2017-07-12 06:16:37 +0530

    add activities stubs
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/restapi/activities/CommentsActivity.java
@@ -0,0 +1,15 @@
+package com.codingblocks.restapi.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codingblocks.restapi.R;
+
+public class CommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+    }
+}
---
+++ app/src/main/java/com/codingblocks/restapi/activities/PostsActivity.java
@@ -0,0 +1,15 @@
+package com.codingblocks.restapi.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codingblocks.restapi.R;
+
+public class PostsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts);
+    }
+}
