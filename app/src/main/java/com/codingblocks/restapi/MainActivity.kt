36a18a9664bee1cd2051e35ec0bc8fce126a088e
Arnav Gupta <arnav@codingblocks.com>
2017-07-12 21:54:28 +0530

    mainactivity minified
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/restapi/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.codingblocks.restapi
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+
+import com.codingblocks.restapi.activities.AlbumsActivity
+import com.codingblocks.restapi.activities.PostsActivity
+import com.codingblocks.restapi.activities.TodosActivity
+import com.codingblocks.restapi.activities.UsersActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val onButtonClickListener = View.OnClickListener { view ->
+            var i: Intent? = null
+            when (view.id) {
+                R.id.btnAlbums -> i = Intent(this@MainActivity, AlbumsActivity::class.java)
+                R.id.btnPosts -> i = Intent(this@MainActivity, PostsActivity::class.java)
+                R.id.btnTodos -> i = Intent(this@MainActivity, TodosActivity::class.java)
+                R.id.btnUsers -> i = Intent(this@MainActivity, UsersActivity::class.java)
+            }
+            startActivity(i)
+        }
+
+        btnTodos.setOnClickListener(onButtonClickListener)
+        btnAlbums.setOnClickListener(onButtonClickListener)
+        btnPosts.setOnClickListener(onButtonClickListener)
+        btnUsers.setOnClickListener(onButtonClickListener)
+
+    }
+}
